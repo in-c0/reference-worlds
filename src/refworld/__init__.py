f66850ec277359c1444bebc77a6db812c6f9f399
ava kim <wldud5192@gmail.com>
2026-09-01 12:18:57 +1000

    feat: export strict reporting helpers

--- src/refworld/__init__.py
+++ src/refworld/__init__.py
@@ -10,6 +10,7 @@ from .metrics import (
     summarize_relative_revisit,
 )
 from .registration import RegistrationResult, project_world_points, recover_camera_pnp
+from .reporting import json_safe, load_json, validate_report, write_report
 
 __all__ = [
     "AnchorMetrics",
@@ -17,9 +18,13 @@ __all__ = [
     "RelativeRevisitSummary",
     "RegistrationResult",
     "anchor_metrics",
+    "json_safe",
+    "load_json",
     "psnr",
     "project_world_points",
     "recover_camera_pnp",
     "summarize_curve",
     "summarize_relative_revisit",
+    "validate_report",
+    "write_report",
 ]
