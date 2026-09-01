b08d03fdd3683d918d2aa6d347fa9a6c8814c177
ava kim <wldud5192@gmail.com>
2026-09-01 10:46:47 +1000

    feat: export adapter protocol types

---
+++ src/refworld/adapters/__init__.py
@@ -0,0 +1,10 @@
+from .base import Camera, ExportBundle, RenderFrame, Unsupported, WorldAdapter, WorldRef
+
+__all__ = [
+    "Camera",
+    "ExportBundle",
+    "RenderFrame",
+    "Unsupported",
+    "WorldAdapter",
+    "WorldRef",
+]
