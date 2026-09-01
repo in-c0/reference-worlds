c861352725c962d0f078cf1fc426ca8d07d6a416
ava kim <wldud5192@gmail.com>
2026-09-01 12:14:29 +1000

    feat: export camera registration primitives

--- src/refworld/__init__.py
+++ src/refworld/__init__.py
@@ -9,13 +9,17 @@ from .metrics import (
     summarize_curve,
     summarize_relative_revisit,
 )
+from .registration import RegistrationResult, project_world_points, recover_camera_pnp
 
 __all__ = [
     "AnchorMetrics",
     "CurveSummary",
     "RelativeRevisitSummary",
+    "RegistrationResult",
     "anchor_metrics",
     "psnr",
+    "project_world_points",
+    "recover_camera_pnp",
     "summarize_curve",
     "summarize_relative_revisit",
 ]
